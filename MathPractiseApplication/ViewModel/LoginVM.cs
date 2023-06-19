5b8eae30442ae812b6c3151f57b29809ba7194cc
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-19 11:06:07 +0300

    Add LoginViewModel

---
+++ MathPractiseApplication/ViewModel/LoginVM.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MathPractiseApplication.ViewModel
+{
+    class LoginVM : ViewModelBase
+    {
+        private string _username;
+        private string _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public string Password
+        {
+            get => _password; 
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get => _isViewVisible; 
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get => _errorMessage; 
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand ShowPassword { get; }
+
+        public LoginVM()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData = true;
+            if (string.IsNullOrWhiteSpace(Username) || Password == null || Password.Length < 3)
+                validData = false;
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            
+        }
+    }
+}
