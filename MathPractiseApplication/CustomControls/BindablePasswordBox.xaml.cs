18ee4d861fc192c7675b07548efa6b5e446916c6
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-19 10:54:33 +0300

    Add custom password box

---
+++ MathPractiseApplication/CustomControls/BindablePasswordBox.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathPractiseApplication.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        public static readonly DependencyProperty PasswordProperty = 
+            DependencyProperty.Register("Password",typeof(string),typeof(BindablePasswordBox));
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty);}
+            set { SetValue(PasswordProperty, value); }
+        }
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+            UserPassword.PasswordChanged += OnPasswordChange;
+        }
+
+        private void OnPasswordChange(object sender, RoutedEventArgs e)
+        {
+            Password += UserPassword.Password;
+        }
+    }
+}
