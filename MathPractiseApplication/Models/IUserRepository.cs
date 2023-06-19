8d6331751f72e1b27ca86a570799200a2355ea66
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-19 11:05:36 +0300

    Add user repository

---
+++ MathPractiseApplication/Models/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPractiseApplication.Models
+{
+    public interface IUserRepository
+    {
+        bool AuthenticateUser(NetworkCredential credential);
+        void Add(User user);
+        void Edit(User user);
+        void Remove(int id);
+        User UserGetById(int id);
+        User UserGetByName(string username);
+    }
+}
