7b315ede492cb5ff8eb2efb111ef3a8c1b34c94e
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-19 13:07:35 +0300

    Change Startup window

--- MathPractiseApplication/App.xaml.cs
+++ MathPractiseApplication/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using MathPractiseApplication.View;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,19 @@ namespace MathPractiseApplication
     /// </summary>
     public partial class App : Application
     {
+        protected void ApplicationStart(object sender, EventArgs e)
+        {
+            var AuthorizationView = new AuthorizationView();
+            AuthorizationView.Show();
+            AuthorizationView.IsVisibleChanged += (s, ev) =>
+            {
+                if (AuthorizationView.IsVisible == false && AuthorizationView.IsLoaded)
+                {
+                    var mainView = new Main();
+                    mainView.Show();
+                    AuthorizationView.Close();
+                }
+            };
+        }
     }
 }
