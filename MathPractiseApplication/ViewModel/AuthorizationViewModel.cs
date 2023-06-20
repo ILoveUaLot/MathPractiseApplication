5584175efb7c9f3addfaa837ea69d3d03425d65d
dmitriylistopad <dimalist2005@gmail.com>
2023-06-20 17:14:00 +0300

    add viewvisible property

--- MathPractiseApplication/ViewModel/AuthorizationViewModel.cs
+++ MathPractiseApplication/ViewModel/AuthorizationViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace MathPractiseApplication.ViewModel
@@ -10,7 +12,7 @@ namespace MathPractiseApplication.ViewModel
     public class AuthorizationViewModel : ViewModelBase
     {
         private ViewModelBase _childView;
-
+        private bool _isViewVisible = true;
         public ViewModelBase CurentChildView 
         { 
             get => _childView;
@@ -18,6 +20,15 @@ namespace MathPractiseApplication.ViewModel
             {
                 _childView = value;
                 OnPropertyChanged(nameof(CurentChildView));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get => _isViewVisible;
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
             }
         }
 
@@ -36,6 +47,7 @@ namespace MathPractiseApplication.ViewModel
         private void ExecuteShowRegisterViewCommand(object obj)
         {
             CurentChildView = new RegisterViewModel();
+            
         }
 
         private void ExecuteShowLoginViewCommand(object obj)
