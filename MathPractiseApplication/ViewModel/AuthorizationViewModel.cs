2102b6615a7eb43857f6785232cbde5f01234165
dmitriylistopad <dimalist2005@gmail.com>
2023-06-20 09:58:20 +0300

    Add VM for auth

---
+++ MathPractiseApplication/ViewModel/AuthorizationViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MathPractiseApplication.ViewModel
+{
+    public class AuthorizationViewModel : ViewModelBase
+    {
+        private ViewModelBase _childView;
+
+        public ViewModelBase CurentChildView 
+        { 
+            get => _childView;
+            set
+            {
+                _childView = value;
+                OnPropertyChanged(nameof(CurentChildView));
+            }
+        }
+
+        public ICommand ShowLoginViewCommand { get; }
+        public ICommand ShowRegisterViewCommand { get; }
+
+        public AuthorizationViewModel()
+        {
+            ShowLoginViewCommand = new ViewModelCommand(ExecuteShowLoginViewCommand);
+            ShowRegisterViewCommand = new ViewModelCommand(ExecuteShowRegisterViewCommand);
+
+            //Set default view
+            ExecuteShowLoginViewCommand(null);
+        }
+
+        private void ExecuteShowRegisterViewCommand(object obj)
+        {
+            CurentChildView = new RegisterViewModel();
+        }
+
+        private void ExecuteShowLoginViewCommand(object obj)
+        {
+            CurentChildView = new LoginVM();
+        }
+    }
+}
