99584cd5a62a5b9819f86fd7c3e054b025e4a797
dmitriylistopad <dimalist2005@gmail.com>
2023-06-20 09:58:29 +0300

    Add VM for registration

---
+++ MathPractiseApplication/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MathPractiseApplication.ViewModel
+{
+    public class RegisterViewModel : ViewModelBase
+    {
+        private string _username;
+        private string _password;
+        private string _repeatedPassword;
+
+        public string Username
+        {
+            get => _username; 
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public string Password
+        {
+            get => _password; 
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string RepeatedPassword
+        {
+            get => _repeatedPassword; 
+            set
+            {
+                _repeatedPassword = value;
+                OnPropertyChanged(nameof(RepeatedPassword));
+            }
+        }
+
+        public ICommand RegistrationCommand { get; }
+
+        private bool CanExecuteRegistrationCommand(object parameter)
+        {
+            bool validData = true;
+            if (string.IsNullOrWhiteSpace(Username) || Password == null || Password.Length < 3)
+                validData = false;
+            return validData;
+        }
+    }
+}
