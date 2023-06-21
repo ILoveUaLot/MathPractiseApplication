2024b7f97b0a0fba1f85cd0cb9c680b090d1d6d7
dmitriylistopad <dimalist2005@gmail.com>
2023-06-21 23:01:34 +0300

    Add useracc model

---
+++ MathPractiseApplication/Models/UserAccountModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPractiseApplication.Models
+{
+    public class UserAccountModel
+    {
+        public string UserName { get; set; }
+        public string DisplayName { get; set; }
+        public byte[] ProfilePicture { get; set; }
+    }
+}
