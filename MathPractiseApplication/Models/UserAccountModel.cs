5bea774d6256471a18e7ff5e3c2f81bd97972657
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-21 23:58:03 +0300

    Remove unused field from useracc model

--- MathPractiseApplication/Models/UserAccountModel.cs
+++ MathPractiseApplication/Models/UserAccountModel.cs
@@ -9,7 +9,6 @@ namespace MathPractiseApplication.Models
     public class UserAccountModel
     {
         public string UserName { get; set; }
-        public string DisplayName { get; set; }
         public byte[] ProfilePicture { get; set; }
     }
 }
