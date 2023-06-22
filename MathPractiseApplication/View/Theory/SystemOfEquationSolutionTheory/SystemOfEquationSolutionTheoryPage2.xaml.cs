2a9a105cf951c5fc973fb3b42254d52062a6ff13
dmitriylistopad <dimalist2005@gmail.com>
2023-06-22 15:17:19 +0300

    Add click handlers

--- MathPractiseApplication/View/Theory/SystemOfEquationSolutionTheory/SystemOfEquationSolutionTheoryPage2.xaml.cs
+++ MathPractiseApplication/View/Theory/SystemOfEquationSolutionTheory/SystemOfEquationSolutionTheoryPage2.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathPractiseApplication.View.Theory.LinearEquationTheory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace MathPractiseApplication.View.Theory.SystemOfEquationSolutionTheory
         public SystemOfEquationSolutionTheoryPage2()
         {
             InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new SystemOfEquationSolutionTheoryPage1());
         }
     }
 }
