8a81945ca2c25ccd3f17e7324588f17882a2bc65
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-22 04:22:54 +0300

    Add ICommand for chosing linearequation theory

---
+++ MathPractiseApplication/ViewModel/ChooseTheoryViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MathPractiseApplication.View.Theory;
+using MathPractiseApplication.View.Theory.LinearEquationTheory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MathPractiseApplication.ViewModel
+{
+    public class ChooseTheoryViewModel : ViewModelBase
+    {
+        private Page _currentViewTheory;
+        public Page CurrentViewTheory
+        {
+            get => _currentViewTheory; 
+            set
+            {
+                _currentViewTheory = value;
+                OnPropertyChanged(nameof(CurrentViewTheory));
+            }
+        }
+
+        public ICommand ShowLinearEquationTheoryPageCommand { get; }
+
+        public void ExecuteShowLinearEquationTheoryPageCommand(object obj)
+        {
+            CurrentViewTheory = new LinearEquationTheoryPage1();
+        }
+        public ChooseTheoryViewModel()
+        {
+            ShowLinearEquationTheoryPageCommand = new ViewModelCommand(ExecuteShowLinearEquationTheoryPageCommand);
+        }
+    }
+}
