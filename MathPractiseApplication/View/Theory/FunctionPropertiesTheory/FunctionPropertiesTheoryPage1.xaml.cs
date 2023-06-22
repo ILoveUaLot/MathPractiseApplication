d7d297be73ac013e0fed6deec0c816479e9a9112
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-22 05:24:44 +0300

    Add theoryviews pages

---
+++ MathPractiseApplication/View/Theory/FunctionPropertiesTheory/FunctionPropertiesTheoryPage1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MathPractiseApplication.View.Theory.LinearEquationTheory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathPractiseApplication.View.Theory.FunctionPropertiesTheory
+{
+    /// <summary>
+    /// Interaction logic for FunctionPropertiesTheoryPage1.xaml
+    /// </summary>
+    public partial class FunctionPropertiesTheoryPage1 : Page
+    {
+        public FunctionPropertiesTheoryPage1()
+        {
+            InitializeComponent();
+        }
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new FunctionPropertiesTheoryPage2());
+        }
+    }
+}
---
+++ MathPractiseApplication/View/Theory/FunctionPropertiesTheory/FunctionPropertiesTheoryPage4.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MathPractiseApplication.View.Theory.LinearEquationTheory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathPractiseApplication.View.Theory.FunctionPropertiesTheory
+{
+    /// <summary>
+    /// Interaction logic for FunctionPropertiesTheoryPage4.xaml
+    /// </summary>
+    public partial class FunctionPropertiesTheoryPage4 : Page
+    {
+        public FunctionPropertiesTheoryPage4()
+        {
+            InitializeComponent();
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new FunctionPropertiesTheoryPage3());
+        }
+    }
+}
