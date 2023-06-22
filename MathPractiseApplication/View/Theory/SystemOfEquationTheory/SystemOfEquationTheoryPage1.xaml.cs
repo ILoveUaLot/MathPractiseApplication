f26c536faf4a17e6e2489bb46aaab19b7156342e
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-22 06:58:03 +0300

    Add theory for systemqeuation

---
+++ MathPractiseApplication/View/Theory/SystemOfEquationTheory/SystemOfEquationTheoryPage1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathPractiseApplication.View.Theory.SystemOfEquationTheory
+{
+    /// <summary>
+    /// Interaction logic for SystemOfEquationTheoryPage1.xaml
+    /// </summary>
+    public partial class SystemOfEquationTheoryPage1 : Page
+    {
+        public SystemOfEquationTheoryPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
