73d9d0bba9f2dbaf5240b07a860aef75f64c523b
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-22 06:00:11 +0300

    Add command for call theory view

--- MathPractiseApplication/ViewModel/ChooseTheoryViewModel.cs
+++ MathPractiseApplication/ViewModel/ChooseTheoryViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MathPractiseApplication.View.Theory;
+using MathPractiseApplication.View.Theory.FunctionPropertiesTheory;
 using MathPractiseApplication.View.Theory.LinearEquationTheory;
 using System;
 using System.Collections.Generic;
@@ -24,14 +25,20 @@ namespace MathPractiseApplication.ViewModel
         }
 
         public ICommand ShowLinearEquationTheoryPageCommand { get; }
+        public ICommand ShowFunctionPropertiesTheoryPageCommand { get; }
 
         public void ExecuteShowLinearEquationTheoryPageCommand(object obj)
         {
             CurrentViewTheory = new LinearEquationTheoryPage1();
         }
+        public void ExecuteShowFunctionPropertiesTheoryPageCommand(object obj)
+        {
+            CurrentViewTheory = new FunctionPropertiesTheoryPage1();
+        }
         public ChooseTheoryViewModel()
         {
             ShowLinearEquationTheoryPageCommand = new ViewModelCommand(ExecuteShowLinearEquationTheoryPageCommand);
+            ShowFunctionPropertiesTheoryPageCommand = new ViewModelCommand(ExecuteShowFunctionPropertiesTheoryPageCommand);
         }
     }
 }
