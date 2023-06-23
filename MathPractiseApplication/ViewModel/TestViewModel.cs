ab7db0027ce99c4484e1b74843100411071c6030
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-23 20:34:12 +0300

    Add checking right answers

--- MathPractiseApplication/ViewModel/TestViewModel.cs
+++ MathPractiseApplication/ViewModel/TestViewModel.cs
@@ -15,69 +15,101 @@ namespace MathPractiseApplication.ViewModel
         private ITestQuestionsRepository _questionsRepository;
         private TestModel _currentQuestion;
         private int _currentQuestionIndex;
+        private int _correctAnswers = 0;
+        private int _selectedAnswer;
+        private Dictionary<TestModel, int> _userAnswers;
 
+        public string AnsweredQuestionInfo => $"{CurrentQuestionIndex}/{questions.Count}";
         public List<TestModel> Questions
         {
-            get => questions; 
+            get => questions;
             set
             {
                 questions = value;
                 OnPropertyChanged(nameof(Questions));
             }
         }
-
-        public TestModel CurrentQuestion
-        {
-            get => _currentQuestion; 
-            set
-            {
-                _currentQuestion = value;
-                OnPropertyChanged(nameof(CurrentQuestion));
-            }
-        }
-
         public int CurrentQuestionIndex
         {
-            get => _currentQuestionIndex; 
+            get => _currentQuestionIndex;
             set
             {
                 _currentQuestionIndex = value;
                 OnPropertyChanged(nameof(CurrentQuestionIndex));
             }
         }
+        public int SelectedAnswer
+        {
+            get => _selectedAnswer;
+            set
+            {
+                _selectedAnswer = value;
+                if (_currentQuestion != null)
+                {
+                    _userAnswers[_currentQuestion] = value;
+                }
+                OnPropertyChanged(nameof(SelectedAnswer));
+            }
+        }
+        public TestModel CurrentQuestion
+        {
+            get => _currentQuestion;
+            set
+            {
+                _currentQuestion = value;
+                if (_userAnswers.ContainsKey(value))
+                {
+                    SelectedAnswer = _userAnswers[value];
+                }
+                else
+                {
+                    _userAnswers.Add(value, -1);
+                }
+                OnPropertyChanged(nameof(CurrentQuestion));
+            }
+        }
 
         public TestViewModel()
         {
+            _userAnswers = new Dictionary<TestModel, int>();
+            GetNextQuestion = new ViewModelCommand(ExecuteGetNextQuestionCommand, CanExecuteNextQuestionCommand);
+            GetPreviousQuestion = new ViewModelCommand(ExecuteGetPreviousQuestion, CanExecuteGetPreviousQuestion);
             _questionsRepository = new TestQuestionExcelRepository();
             LoadQuestions();
-            CurrentQuestionIndex = 0;
-            if(questions.Count > 0)
-            {
-                CurrentQuestion = questions[CurrentQuestionIndex];
-            }
         }
 
         private void LoadQuestions()
         {
-            Questions = _questionsRepository.GetQuestions();
+            var allQuestions = _questionsRepository.GetQuestions();
+            if (allQuestions.Count > 0)
+            {
+                var rnd = new Random();
+                Questions = allQuestions.OrderBy(x => rnd.Next()).Take(20).ToList();
+                CurrentQuestion = Questions[0];
+                _currentQuestionIndex = 0;
+            }
+            else
+            {
+                throw new InvalidOperationException("Not enough questions in the repository");
+            }
         }
 
         public ICommand GetNextQuestion { get; }
 
         public ICommand GetPreviousQuestion { get; }
+
+
         private void ExecuteGetPreviousQuestion(object obj)
         {
             CurrentQuestionIndex--;
             CurrentQuestion = questions[CurrentQuestionIndex];
         }
+
         private bool CanExecuteGetPreviousQuestion(object obj)
         {
-            if (CurrentQuestionIndex > 0)
-            {
-                return true;
-            }
-            return false;
+            return CurrentQuestionIndex > 0;
         }
+
         private void ExecuteGetNextQuestionCommand(object obj)
         {
             CurrentQuestionIndex++;
@@ -86,11 +118,13 @@ namespace MathPractiseApplication.ViewModel
 
         private bool CanExecuteNextQuestionCommand(object obj)
         {
-            if(CurrentQuestionIndex < questions.Count - 1)
-            {
-                return true;
-            }
-            return false;
+            return CurrentQuestionIndex < questions.Count - 1;
+        }
+
+        public int CalculateCorrectAnswers()
+        {
+            _correctAnswers = _userAnswers.Count(kvp => kvp.Key.IndexOfRightAnswer == kvp.Value);
+            return _correctAnswers;
         }
     }
 }
