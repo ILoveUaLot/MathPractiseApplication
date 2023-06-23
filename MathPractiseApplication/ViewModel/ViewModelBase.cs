d7928fa6c03d3f485b86324c92205a8bb3eae8a1
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-23 22:31:58 +0300

    Add SetProperty to viewmodel base

--- MathPractiseApplication/ViewModel/ViewModelBase.cs
+++ MathPractiseApplication/ViewModel/ViewModelBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +15,15 @@ namespace MathPractiseApplication.ViewModel
         public void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 }
