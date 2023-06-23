52dad9fa5cf957c3f7b9bc1eb0cd36b669ad0c1c
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-23 03:41:22 +0300

    Add interface

---
+++ MathPractiseApplication/Services/IPractiseService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathPractiseApplication.Models;
+
+namespace MathPractiseApplication.Services
+{
+    public interface IPractiseService
+    {
+        PractiseModel GenerateQuestion();
+        bool CheckAnswer(PractiseModel question, double userAnswer);
+    }
+}
