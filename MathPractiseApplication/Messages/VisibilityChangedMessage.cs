3cc6dd89b9e745744ed667c9da9c58697677941d
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-24 03:15:40 +0300

    Add message class

---
+++ MathPractiseApplication/Messages/VisibilityChangedMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPractiseApplication.Messages
+{
+    public class VisibilityChangedMessage
+    {
+        public bool IsVisible { get; }
+
+        public VisibilityChangedMessage(bool isVisible)
+        {
+            IsVisible = isVisible;
+        }
+    }
+}
