6a397c9655c948146816b0e839b6801945e9a4c0
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-24 07:59:34 +0300

    added methods

--- MathPractiseApplication/Repositories/UserExcelRepository.cs
+++ MathPractiseApplication/Repositories/UserExcelRepository.cs
@@ -13,7 +13,7 @@ namespace MathPractiseApplication.Repositories
     public class UserExcelRepository : IUserRepository
     {
         ExcelPackage userPackage;
-        string filePath ="Data/Users.xlsx";
+        string filePath = "Data/Users.xlsx";
 
         public UserExcelRepository()
         {
@@ -30,6 +30,8 @@ namespace MathPractiseApplication.Repositories
                 worksheetUsers.Cells[1, 1].Value = "Id";
                 worksheetUsers.Cells[1, 2].Value = "Name";
                 worksheetUsers.Cells[1, 3].Value = "Password";
+                worksheetUsers.Cells[1, 4].Value = "CompletedExercises";  // new column for completed exercises
+                worksheetUsers.Cells[1, 5].Value = "TestResults";  // new column for test results
                 userPackage.SaveAs(new FileInfo(filePath));
             }
         }
@@ -41,7 +43,39 @@ namespace MathPractiseApplication.Repositories
             worksheetUsers.Cells[lastUsedRow + 1, 1].Value = lastUsedRow;
             worksheetUsers.Cells[lastUsedRow + 1, 2].Value = user.Name;
             worksheetUsers.Cells[lastUsedRow + 1, 3].Value = user.Password;
+            worksheetUsers.Cells[lastUsedRow + 1, 4].Value = user.CompletedExercises;  // save completed exercises
+            worksheetUsers.Cells[lastUsedRow + 1, 5].Value = user.TestResults;  // save test results
             userPackage.SaveAs(new FileInfo(filePath));
+        }
+
+        public User UserGetById(int id)
+        {
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheetUsers = package.Workbook.Worksheets["Users"];
+                int totalRows = worksheetUsers.Dimension.End.Row;
+
+                for (int row = 2; row <= totalRows; row++)
+                {
+                    int userId = Convert.ToInt32(worksheetUsers.Cells[row, 1].Value);
+                    if (userId == id)
+                    {
+                        string name = worksheetUsers.Cells[row, 2].Value.ToString();
+                        string password = worksheetUsers.Cells[row, 3].Value.ToString();
+                        int completedExercises = Convert.ToInt32(worksheetUsers.Cells[row, 4].Value); // load completed exercises
+                        string testResults = worksheetUsers.Cells[row, 5].Value.ToString();  // load test results
+                        return new User
+                        {
+                            Id = userId,
+                            Name = name,
+                            Password = password,
+                            CompletedExercises = completedExercises,
+                            TestResults = testResults
+                        };
+                    }
+                }
+            }
+            return null;
         }
 
         public bool AuthenticateUser(NetworkCredential credential)
@@ -108,27 +142,6 @@ namespace MathPractiseApplication.Repositories
             }
         }
 
-        public User UserGetById(int id)
-        {
-            using (var package = new ExcelPackage(new FileInfo(filePath)))
-            {
-                var worksheetUsers = package.Workbook.Worksheets["Users"];
-                int totalRows = worksheetUsers.Dimension.End.Row;
-
-                for (int row = 2; row <= totalRows; row++)
-                {
-                    int userId = Convert.ToInt32(worksheetUsers.Cells[row, 1].Value);
-                    if (userId == id)
-                    {
-                        string name = worksheetUsers.Cells[row, 2].Value.ToString();
-                        string password = worksheetUsers.Cells[row, 3].Value.ToString();
-                        return new User { Id = userId, Name = name, Password = password };
-                    }
-                }
-            }
-            return null;
-        }
-
         public User UserGetByName(string username)
         {
             using (var package = new ExcelPackage(new FileInfo(filePath)))
@@ -148,6 +161,39 @@ namespace MathPractiseApplication.Repositories
                 }
             }
             return null;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            var users = new List<User>();
+
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheetUsers = package.Workbook.Worksheets["Users"];
+                int totalRows = worksheetUsers.Dimension.End.Row;
+
+                for (int row = 2; row <= totalRows; row++)
+                {
+                    int id = Convert.ToInt32(worksheetUsers.Cells[row, 1].Value);
+                    string name = worksheetUsers.Cells[row, 2].Value.ToString();
+                    string password = worksheetUsers.Cells[row, 3].Value.ToString();
+                    int practiceTasksCount = Convert.ToInt32(worksheetUsers.Cells[row, 4].Value);
+                    string testScore = worksheetUsers.Cells[row, 5].Value.ToString();
+
+                    var user = new User
+                    {
+                        Id = id,
+                        Name = name,
+                        Password = password,
+                        CompletedExercises = practiceTasksCount,
+                        TestResults = testScore
+                    };
+
+                    users.Add(user);
+                }
+            }
+
+            return users;
         }
     }
 }
