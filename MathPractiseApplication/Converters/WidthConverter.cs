695230a0e3adec9cea9772b46466bd3602f005cb
Dmitriy Listopad <dimalist2005@gmail.com>
2023-06-25 16:43:24 +0300

    Add width converter from procents to width

---
+++ MathPractiseApplication/Converters/WidthConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MathPractiseApplication.Converters
+{
+    public class WidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double percent = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+            double width = (double)value;
+
+            return width * percent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
